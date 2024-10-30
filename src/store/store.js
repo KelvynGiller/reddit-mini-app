1315b746a441a85f3bcd7bd5ea9aa03b83a6b406
Kelvyn Giller <kelvyngiller9@gmail.com>
2024-10-30 11:14:02 -0300

    added Provider to index.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import postsReducer from '../slices/postsSlice';
 
 const store = configureStore({
     reducer: {
-
+        posts: postsReducer
     }
 });
 
