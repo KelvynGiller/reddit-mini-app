4845ab25fac0fc97c63748761353a757fa29c434
Kelvyn Giller <kelvyngiller9@gmail.com>
2024-10-31 14:52:15 -0300

    added subreddit categories

---
+++ src/components/SubredditList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from '../style/SubredditList.module.css';
+
+const SubredditList = () => {
+    const subreddits = [
+        'Home',
+        'AskReddit',
+        'NoStupidQuestions',
+        'BaldursGate3',
+        'facepalm',
+        'interestingasfuck',
+        'Damnthatsinteresting',
+        'LivestreamFail',
+        'pics',
+        'AmItheAsshole' 
+    ];
+
+    return (
+        <div className={styles.SubredditList}>
+            <h2>Subreddits</h2>
+            <ul>
+                {subreddits.map((subreddit) => (
+                    <li key={subreddit}>
+                        {subreddit}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default SubredditList;
