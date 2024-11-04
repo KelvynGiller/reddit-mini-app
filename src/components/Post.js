7d824f2a407dca86ce22a1a54307ec92db471a13
Kelvyn Giller <kelvyngiller9@gmail.com>
2024-11-04 11:31:32 -0300

    implemeted comments button

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../style/Post.module.css';
 import { Link } from 'react-router-dom';
 
 const Post = ({ post }) => {
+    const [showComments, setShowComments] = useState(false);
+    const postDate = new Date(post.data.created_utc * 1000).toLocaleDateString();
+
+    const handleCommentsToggle = () => {
+        setShowComments(!showComments);
+    }
+
+
     return (
         <div className={styles.post}>
             <Link to={`/post/${post.data.id}`}>
                 <h3 className={styles.title}>{post.data.title}</h3>
             </Link>
+            <p>Author: {post.data.author} | Posted: {postDate}</p>
             {post.data.url && (
                 <img 
                     src={post.data.url} 
@@ -16,6 +25,14 @@ const Post = ({ post }) => {
                 />
             )}
             <p>{post.data.selftext}</p>
+            <button onClick={handleCommentsToggle}>
+                {showComments ? 'Hide Comments' : 'Show Comments'}
+            </button>
+            {showComments && (
+                <div className={styles.comments}>
+                    <p>comments</p>
+                </div>
+            )}
         </div>
     );
 };
