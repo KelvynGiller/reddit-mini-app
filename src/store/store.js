ac5fc54e889dd4cf208d6f150d202fce09f6b647
Kelvyn Giller <kelvyngiller9@gmail.com>
2024-11-05 09:34:40 -0300

    removed posts details slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from '../slices/postsSlice';
-import postsDetailsReducer from '../slices/postsDetailsSlice';
+
 
 const store = configureStore({
     reducer: {
         posts: postsReducer,
-        postsDetails: postsDetailsReducer,
     },
 });
 
